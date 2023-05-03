aff60598e49717a089ca12e0477ef6457b643faa
micaela quijano <micaquijano04@gmail.com>
2023-05-03 16:00:30 -0300

    instalacion de material

--- ProyectoDeAngular/src/app/app.module.ts
+++ ProyectoDeAngular/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/header/header.component';
 import { HeaderModule } from 'src/header/header.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule],
+  imports: [BrowserModule, HeaderModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
 
+ 
