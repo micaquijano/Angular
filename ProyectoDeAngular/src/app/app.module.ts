0001501f427fe1a4450ab43f0bf330fd284180ff
micaela quijano <micaquijano04@gmail.com>
2023-05-03 20:20:50 -0300

    propiedad isDarkThemeActive

--- ProyectoDeAngular/src/app/app.module.ts
+++ ProyectoDeAngular/src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/header/header.component';
 import { HeaderModule } from 'src/header/header.module';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule, MatSlideToggleModule],
+  imports:[
+  BrowserModule,
+  HeaderModule, 
+  MatSlideToggleModule,
+  FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
