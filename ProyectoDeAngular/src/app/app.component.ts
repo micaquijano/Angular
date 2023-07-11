4ab9f82ed16166acdc3d5ab97f2dfe291f3f5ffa
micaela quijano <micaquijano04@gmail.com>
2023-07-10 23:38:23 -0300

    boton modo oscuro

--- ProyectoDeAngular/src/app/app.component.ts
+++ ProyectoDeAngular/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 
 @Component({
@@ -7,26 +6,8 @@ import { Component, Inject } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isDarkThemeActive = true;
   title = 'ProyectoDeAngular';
   flagDelHijo: boolean = false;
   buttonColor = 'accent';
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
-    this.document.body.classList.add('theme-dark');
-  }
-
-  onchance(newValue: boolean): void {
-    //ejecutar cuando una situación ocurre.
-    this.isDarkThemeActive = !this.isDarkThemeActive;
-    if (newValue) {
-      this.buttonColor = 'accent';
-      this.document.body.classList.add('theme-dark');
-      this.document.body.classList.remove('theme-light');
-    } else {
-      this.buttonColor = 'primary';
-      this.document.body.classList.add('theme-light');
-      this.document.body.classList.remove('theme-dark');
-    }
-  }
 }
--- ProyectoDeAngular/src/app/themeButton/themeButton.component.ts
+++ ProyectoDeAngular/src/app/themeButton/themeButton.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 const darkTheme = { tooltip: 'modo oscuro', icon: 'dark_mode' };
 const lightTheme = { tooltip: 'modo claro', icon: 'sunny' };
@@ -10,10 +11,10 @@ const lightTheme = { tooltip: 'modo claro', icon: 'sunny' };
 })
 export class ThemeButtonComponent {
 /*   theme = true; */
+  isDarkThemeActive = true;
   themeObj = darkTheme;
-
-  constructor() {}
-
+  buttonColor = 'accent'; 
+ 
 /*   changeTheme() {
     console.log(this.theme);
     this.theme = !this.theme;
@@ -24,6 +25,27 @@ export class ThemeButtonComponent {
   } */
 
   changeThemeRefactor() {
-    this.themeObj = this.themeObj.icon !=  darkTheme.icon  ? darkTheme : lightTheme;
+    /*this.themeObj = this.themeObj.icon !=  darkTheme.icon  ? darkTheme : lightTheme;*/
+    this.themeObj = this.themeObj.icon != darkTheme.icon ? darkTheme : lightTheme  
+  }
+
+
+
+  
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.document.body.classList.add('theme-dark');
+  }
+  onchance(newValue: boolean): void {
+    //ejecutar cuando una situación ocurre.
+    this.isDarkThemeActive = !this.isDarkThemeActive;
+    if (newValue) {
+      this.buttonColor = 'accent';
+      this.document.body.classList.add('theme-dark');
+      this.document.body.classList.remove('theme-light');
+    } else {
+      this.buttonColor = 'primary';
+      this.document.body.classList.add('theme-light');
+      this.document.body.classList.remove('theme-dark');
+    }
   }
 }
